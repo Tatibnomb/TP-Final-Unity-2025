32d47ebe301e244433554f85d768456fc1c26e45
Tatibnomb <49379010@est.ort.edu.ar>
2025-11-28 09:03:22 -0300

    materiales de la granja agregados

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 5f;
+    public float speed = 60f;
     public float sensitivity = 2f;
 
     private Rigidbody rb;
